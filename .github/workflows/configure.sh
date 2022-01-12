9a7a6bd4c0c865b4f471149ff7707e73c5289aef
Govind Chari <govindchari@Govinds-MacBook-Pro.local>
2022-01-11 23:19:01 -0500

    yeet

--- .github/workflows/configure.sh
+++ .github/workflows/configure.sh
@@ -1,1 +1,1 @@
-(cd lib/eigen/ && rm -rf build/ && mkdir build && cd build/ && cmake -G "Unix Makefiles" .. && cmake --build .)
+(cd lib/eigen/ && rm -rf build/ && ls && mkdir build && cd build/ && cmake -G "Unix Makefiles" .. && cmake --build .)
