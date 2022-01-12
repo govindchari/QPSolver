66b88f558e3a24671074c13c9071ffa1fac856c4
Govind Chari <govindchari@Govinds-MacBook-Pro.local>
2022-01-11 22:13:49 -0500

    Modified build_eigen

--- build_eigen.sh
+++ build_eigen.sh
@@ -1,1 +1,1 @@
-(cd lib/eigen && mkdir build && cd build/ && cmake -G "Unix Makefiles" .. && cmake --build .)
+(cd lib/eigen && rm -rf build && mkdir build && cd build/ && cmake -G "Unix Makefiles" .. && cmake --build .)
